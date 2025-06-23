4c08b63b81b2bd7d98b7ca92101f9c03b7905357
erab2r <shkera213@gmail.com>
2025-06-23 17:59:18 +0600

    fdsfgs

---
+++ binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    int a[n];
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    int val;
+    cin >> val;
+    int flag = 0;
+    int l =0;
+    int r =n-1;
+    while(l<=r){
+        int mid = (l+r)/2;
+        if(a[mid]==val){
+            flag = 1;
+        }
+        else if(a[mid]>val){
+            r = mid - 1;
+        }
+        else{
+            l = mid +1;
+        }
+    }
+    if(flag==1){
+        cout <<"FOUND" <<endl;
+    }
+    else{
+        cout <<"NOT FOUND" <<endl;
+    }
+
+    return 0;
+}
